606ca8565b4cd4a477d14f41c2e105308cc7a1a1
aldls819 <aldls819@gmail.com>
2024-01-18 11:31:14 +0900

    :art: 시멘틱 마크업
    div를 의미있는 태그로 변경

--- frontend/src/DarkModeToggle.js
+++ frontend/src/DarkModeToggle.js
@@ -3,12 +3,14 @@ class DarkModeToggle {
   isDarkMode = null;
 
   constructor({ $target, onSearch }) {
+    const $wrapper = document.createElement("section");
     const $DarkModeToggle = document.createElement("input");
     this.$DarkModeToggle = $DarkModeToggle;
     this.$DarkModeToggle.type = "checkbox";
 
     $DarkModeToggle.className = "DarkModeToggle";
-    $target.appendChild($DarkModeToggle);
+    $wrapper.appendChild($DarkModeToggle);
+    $target.appendChild($wrapper);
 
     $DarkModeToggle.addEventListener("change", (e) => {
       this.setColorMode(e.target.checked);
